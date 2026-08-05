ac5683e547cf635b73144bb8b4ac2ac69d6a3fcc
ismailelomrani22-ops <ismailelomrani22@gmail.com>
2026-08-06 00:51:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import telebot
 
 TOKEN = "8632983518:AAGtdmb-vRqTOMCxo0PGkdxy33872Uqimok"
@@ -7,6 +6,19 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "السلام عليكم! البوت خدام ✅")
+    bot.reply_to(message, "✅ السلام عليكم! البوت خدام")
+
+@bot.message_handler(func=lambda message: True)
+def signals(message):
+    text = message.text.upper()
+
+    if text == "EUR/USD M1":
+        bot.reply_to(message, "📈 BUY")
+
+    elif text == "EUR/USD 5S":
+        bot.reply_to(message, "📉 SELL")
+
+    else:
+        bot.reply_to(message, "❌ الأمر غير معروف")
 
 bot.infinity_polling()
