0d4dc47ed0ef01eedee89dad8c6ac9093dc948d4
ismailelomrani22-ops <ismailelomrani22@gmail.com>
2026-08-06 00:29:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+import telebot
+
+TOKEN = "8632983518:AAGtdmb-vRqTOMCxo0PGkdxy33872Uqimok"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "السلام عليكم! البوت خدام ✅")
+
+bot.infinity_polling()
