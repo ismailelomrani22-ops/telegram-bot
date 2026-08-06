aad33e594f0c6a106af500593542756d7df43066
ismailelomrani22-ops <ismailelomrani22@gmail.com>
2026-08-06 22:03:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
 
+from flask import Flask, render_template, request, jsonify
+from analysis import analyze_market
+
+app = Flask( ai-trading-bot )
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    data = request.get_json()
+
+    pair = data.get("pair")
+    timeframe = data.get("timeframe")
+
+    result = analyze_market(pair, timeframe)
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
