aaffa9965233d3db5f2483acab477f44be60761f
ismailelomrani22-ops <ismailelomrani22@gmail.com>
2026-08-07 06:45:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ def analyze():
 
     result = analyze_market(pair, timeframe)
 
-   print(result)
+    print(result)
 
     return jsonify(result)
-    
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
